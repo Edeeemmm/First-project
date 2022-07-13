d7647e6f643af6bba0a3beac7bf4b606b018c1bb
Edeeemmm <abramovedem@gmail.com>
2022-07-13 16:57:34 +0300

    fix: add js file

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.nav-button');
+const navBtnImg = document.querySelector('.nav-btn-img');
+
+
+navBtn.addEventListener('click', () => {
+    if(nav.classList.toggle('open')){
+        navBtnImg.src = './img/icon/nav-close.png';
+    } else {
+        navBtnImg.src = './img/icon/nav-open.svg';
+    }
+})
+
